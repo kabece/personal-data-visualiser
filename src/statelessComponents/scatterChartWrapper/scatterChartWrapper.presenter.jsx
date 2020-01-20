951e0af797498d4f05b4e55df4b087966f9c6fbb
Chris Czurylo <kabece@gmail.com>
2020-01-21 09:50:47 +1300

    Implemented Tracker for ScatterChart

--- src/statelessComponents/scatterChartWrapper/scatterChartWrapper.presenter.jsx
+++ src/statelessComponents/scatterChartWrapper/scatterChartWrapper.presenter.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {object, string, func} from 'prop-types'
 import {
   Charts,
   ChartContainer,
   ChartRow,
   YAxis,
-  ScatterChart,
-  styler
+  ScatterChart
 } from 'react-timeseries-charts'
 
 import {timeRangeShape} from '../../index.shapes'
@@ -16,55 +15,84 @@ const ScatterChartWrapper = ({
   timeRange,
   chartTitle,
   onSetTimeRange
-}) => (
-  <ChartContainer
-    title={chartTitle}
-    style={{
-      background: '#201d1e',
-      borderRadius: 8,
-      borderStyle: 'solid',
-      borderWidth: 1,
-      borderColor: '#232122'
-    }}
-    padding={20}
-    paddingTop={5}
-    paddingBottom={0}
-    enableDragZoom
-    onTimeRangeChanged={newTimeRange => onSetTimeRange({newTimeRange})}
-    timeRange={timeRange}
-    maxTime={dataSeries.range().end()} // FIXME: maybe figure out how to use that
-    minTime={dataSeries.range().begin()}
-    width={500}
-  >
-    <ChartRow height='200'>
-      <YAxis
-        id='value'
-        min={dataSeries.min('value')}
-        max={dataSeries.max('value')}
-        format=',.0f'
-        width='60'
-        type='linear'
-      />
-      <Charts>
-        <ScatterChart
-          key='value'
-          axis='value'
-          series={dataSeries}
-          columns={['value']}
-          // style={styler}
+}) => {
+  const [highlightedElement, setHighlightedElement] = useState(null)
+
+  const getTrackerValues = () => highlightedElement && [{
+    label: 'Value',
+    value: highlightedElement.event.get(highlightedElement.column)
+  }]
+
+  const trackerStyle = {
+    line: {
+      stroke: 'white',
+      strokeDasharray: 2
+    },
+    box: {
+      fill: 'black'
+    }
+  }
+
+  return (
+    <ChartContainer
+      title={chartTitle}
+      style={{
+        background: '#201d1e',
+        borderRadius: 8,
+        borderStyle: 'solid',
+        borderWidth: 1,
+        borderColor: '#232122',
+        color: 'red'
+      }}
+      padding={20}
+      paddingTop={5}
+      paddingBottom={0}
+      enableDragZoom
+      enablePanZoom
+      onTimeRangeChanged={newTimeRange => onSetTimeRange({newTimeRange})}
+      timeRange={timeRange}
+      maxTime={dataSeries.range().end()} // FIXME: maybe figure out how to use that
+      minTime={dataSeries.range().begin()}
+      width={500}
+    >
+      <ChartRow
+        height='200'
+        trackerShowTime
+      >
+        <YAxis
+          id='value'
+          min={dataSeries.min('value')}
+          max={dataSeries.max('value')}
+          format=',.0f'
+          width='60'
+          type='linear'
         />
-      </Charts>
-      <YAxis
-        id='value'
-        min={dataSeries.min('value')}
-        max={dataSeries.max('value')}
-        format=',.0f'
-        width='60'
-        type='linear'
-      />
-    </ChartRow>
-  </ChartContainer>
-)
+        <Charts>
+          <ScatterChart
+            key='value'
+            axis='value'
+            series={dataSeries}
+            columns={['value']}
+            highlight={highlightedElement}
+            info={getTrackerValues()}
+            infoHeight={30}
+            infoWidth={125}
+            infoStyle={trackerStyle}
+            onMouseNear={newHighlightedElement => setHighlightedElement(newHighlightedElement)}
+          />
+        </Charts>
+        <YAxis
+          id='value'
+          min={dataSeries.min('value')}
+          max={dataSeries.max('value')}
+          format=',.0f'
+          width='60'
+          type='linear'
+        />
+      </ChartRow>
+    </ChartContainer>
+  )
+}
 
 ScatterChartWrapper.propTypes = {
   dataSeries: object.isRequired, // FIXME:
